67bdd16f5246c3dc44e27983bcee959b432d8065
queens kisivuli <queenskisivuli@gmail.com>
2023-05-23 14:03:12 +0300

    main app with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,32 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import "./App.css";
+import api from "./axios/axios";
 
 function App() {
   const [images, setImages] = useState([]);
-  const config = {
-    headers: {
-      Authorization: 'RobVDqZRreoYIwwr0yefoaS4ZbAz1r4z65wjbNLgMYzU7lKseSJc1LyF'
-    },
-    withCredentials: false
-  }
-  const url = "https://api.pexels.com/v1/search?query=nature&per_page=10"
-  
+
   const fetchData = async () => {
-    const data = await axios.get(url, config);
-    setImages(data.data.photos)
-    console.log(data.data.photos);
-  }
-  // fetchData()
+    const data = await api.get("search?query=nature&per_page=10");
+    setImages(data.data.photos);
+  };
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <>
-      {
-       images && images.map((item) => (
+      {images &&
+        images.map((item) => (
           <div key={item.id}>
-            <img src={item.url} alt={item.alt} />
-            <p>Photo taken by <a href={item.photographer_url}>{item.photographer}</a></p>
+            <img src={item.url.medium} alt={item.alt} />
+            <p>
+              Photo taken by{" "}
+              <a href={item.photographer_url}>{item.photographer}</a>
+            </p>
           </div>
-       ))
-
-      }
+        ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
