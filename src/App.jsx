4aa9535a8297da60e8eed93860d39019192ea049
queens kisivuli <queenskisivuli@gmail.com>
2023-05-23 15:11:13 +0300

    show data context

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,65 @@ import { useState, useEffect, createContext } from "react";
 import "./App.css";
 import axios from "axios";
 import Search from "./components/Search";
+import ShowData from "./components/ShowData";
 
 export const SearchTerm = createContext();
 
 function App() {
   const [images, setImages] = useState([]);
-  const [param, setParams] = useState('');
+  const [param, setParams] = useState("");
+  const [clickedItem, setClickedItem] = useState();
   const config = {
     headers: {
       Authorization: "RobVDqZRreoYIwwr0yefoaS4ZbAz1r4z65wjbNLgMYzU7lKseSJc1LyF",
     },
     withCredentials: false,
-    "Acess-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "*",
   };
 
   const fetchData = async (searchterm = "nature") => {
     const url = `https://api.pexels.com/v1/search?query=${searchterm}&per_page=20`;
     const data = await axios.get(url, config);
     setImages(data.data.photos);
-    console.log(data.data.photos);
-    console.log("one");
   };
-  // fetchData()
+
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <>
-      <SearchTerm.Provider value={{ param, setParams, fetchData }}>
-        <Search />
-        <h2>{param}</h2>
-        <div className="images__container">
-          {images &&
-            images.map((item) => (
-              <div key={item.id} className="photo_box">
-                <div className="image_box">
-                  <img src={item.src.large} alt={item.alt} />
-                </div>
-                <p>
-                  Photo taken by{" "}
-                  <a href={item.photographer_url}>{item.photographer}</a>
-                </p>
-              </div>
-            ))}
+      <SearchTerm.Provider value={{ param, setParams, fetchData, clickedItem }}>
+        <div>
+          <Search />
+        </div>
+        <div>
+          <div>
+            <h2>{param}</h2>
+            <div className="images__container">
+              {images &&
+                images.map((item) => (
+                  <div
+                    key={item.id}
+                    className="photo_box"
+                    onClick={() => {
+                      setClickedItem(item);
+                    }}
+                  >
+                    <div className="image_box">
+                      <img src={item.src.large} alt={item.alt} />
+                    </div>
+                    <p>
+                      Photo taken by{" "}
+                      <a href={item.photographer_url}>{item.photographer}</a>
+                    </p>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div>
+            <ShowData />
+          </div>
         </div>
       </SearchTerm.Provider>
     </>
