1c4774442cbd186637791d6e09d644baae840321
queens kisivuli <queenskisivuli@gmail.com>
2023-05-25 14:30:36 +0300

    api key

--- src/axios/axios.jsx
+++ src/axios/axios.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const KEY = import.meta.env.VITE_API_KEY;
+const KEY = import.meta.env.API_KEY;
 
 export const config = {
   headers: {
-    Authorization: KEY,
+    Authorization: "RobVDqZRreoYIwwr0yefoaS4ZbAz1r4z65wjbNLgMYzU7lKseSJc1LyF",
   },
   withCredentials: false,
   "Access-Control-Allow-Origin": "*",
